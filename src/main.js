bd5315f73cf01660d1ee92caf85c9a073e4db4c4
Arkadiusz Klimek <arklimek@student.agh.edu.pl>
2025-05-28 22:30:08 +0200

    ,

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1 class = "text-2xl font-bold text-primary" >Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button" class="bg-blue-500 px-4" ></button>
+    </div>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
